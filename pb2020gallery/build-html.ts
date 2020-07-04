263c677199d6a92d25029fe3a1c79b7c0dc0f2d7
David <thexumaker@berkeley.edu>
2020-07-03 18:28:58 -0700

    Updated Front end for a lightweight flexbox implementation

---
+++ pb2020gallery/build-html.ts
@@ -0,0 +1,46 @@
+
+
+import { promises as fsp } from 'fs';
+import { shotsFolder, outBaseFolder, urlToFname, readJson, dataFile, srcFolder } from './fetch-shots';
+
+const htmlTemplateFile = srcFolder + "/index.template.html";
+const outHtmlFile = outBaseFolder + "/index.html";
+const replacer = 'REPLACETHISPLEASE';
+
+interface Entry {
+  url: string;
+  description: string;
+}
+
+async function main() {
+  const data: Entry[] = await readJson(dataFile);
+  
+  const lines = [];
+  for (const entry of data) {
+    const imageUrl = shotsFolder + '/' + urlToFname(entry.url);
+    lines.push(`
+
+    <div class="card">
+  <div class="cardBody" style= "background-image:url(${imageUrl})">
+    <div class="inner">
+    <a href="${entry.url}" class="site">
+        <p>
+          ${entry.description}
+        </p>
+      </a>
+    </div>
+  </div>
+  </div>
+  `);
+  }
+  
+  const htmlBase = await fsp.readFile(htmlTemplateFile, 'utf8');
+  const outText = htmlBase.replace("REPLACETHISPLEASE", lines.join('\n'));
+  await fsp.writeFile(outHtmlFile, outText);
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+  main()
+    .catch((err) => { console.error("unhandled fail",  __filename, err)})
+    .then(() => { console.log("done with", __filename); });
+}
---
+++ pb2020gallery/fetch-shots.ts
@@ -0,0 +1,116 @@
+import { promises as fsp } from 'fs';
+import * as fs from 'fs';
+
+const rmfr = require('rmfr');
+const captureWebsite = require('capture-website');
+import { ncp } from 'ncp';
+
+export const shotsFolder = 'screenshots';
+export const outBaseFolder = __dirname + '/dist'
+export const outImageFolder = outBaseFolder + '/' + shotsFolder;
+const imagesFilter = outImageFolder + '/*.png';
+export const dataFile = __dirname + '/sites.json';
+export const srcFolder = __dirname + '/src';
+
+
+const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+function linkify(text) {
+
+  return text.replace(urlRegex, function (url) {
+    return '<a href="' + url + '">' + url + '</a>';
+  });
+}
+
+function findAll(reg: RegExp, text: string) {
+  const matches = [];
+  let found;
+  while (found = reg.exec(text)) {
+    matches.push(found[0]);
+    reg.lastIndex -= found[0].split(':')[1].length;
+  }
+  return matches;
+}
+
+export function urlToFname(url: string) {
+  return url.replace(/https?:/g, '').replace(/\//g, '') + '.png';
+}
+
+export async function readJson(filePath: string) {
+  return JSON.parse(await fsp.readFile(filePath, 'utf8'))
+}
+
+function checkFileExists(filepath){
+  return new Promise((resolve, reject) => {
+    fs.access(filepath, fs.constants.F_OK, error => {
+      resolve(!error);
+    });
+  });
+}
+
+async function copyDir(source: string, destination: string) {
+  return new Promise((resolve, reject) => {
+    ncp(source, destination, function (err: Error) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function setupFolder() {
+  // Delete existing images
+  // await rmfr(imagesFilter, { glob: true });
+
+  // Make sure screenshots directory exists
+  if (!await checkFileExists(outImageFolder)) {
+    await fsp.mkdir(outImageFolder, { recursive: true });
+  }
+
+  return copyDir(srcFolder, outBaseFolder);
+}
+
+async function main() {
+  await setupFolder();
+
+  const entries = await readJson(dataFile);
+
+  let promises = [];
+  // maxParallelScreenshots set to something less than the default node
+  // listener limit (15?)
+  const maxParallelScreenshots = 6;
+  for (const entry of entries) {
+    if (promises.length >= maxParallelScreenshots) {
+      // limit parallelism
+      await Promise.all(promises);
+      promises = [];
+    }
+    // console.log(line);
+    const url = entry.url;
+    console.log(url);
+
+    // if (url.indexOf('github.com') >= 0) {
+    //   console.log("skipping github.com");
+    //   continue
+    // }
+    const fname = urlToFname(url);
+    const dest = `${outImageFolder}/${fname}`;
+    promises.push(captureWebsite.file(url, dest, {
+      width: 800,
+      height: 1000,
+      scaleFactor: 1,
+      delay: 5, // seconds delay for the shot because sometimes the red map fails to load
+    }));
+    console.log("promised", dest);
+  }
+  await Promise.all(promises);
+  console.log("done");
+};
+
+if (typeof require !== 'undefined' && require.main === module) {
+  main()
+    .catch((err) => { console.error("unhandled fail", __filename, err) })
+    .then(() => { console.log("done with", __filename); });
+}
